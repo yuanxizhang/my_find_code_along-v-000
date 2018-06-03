37e388401d899582a165f75ae5ef3be53aee1722
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-06-03 02:29:09 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -4,10 +4,10 @@ def my_find(collection)
   i = 0 
   while i < collection.length 
     if yield collection[i]
-      return 
+      return collection[i]
+    end
     i += 1 
   end
   
-  if 
 
 end
